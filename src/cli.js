13b480c3f7dc88aacb49d269ac90b6eeea11fbac
Skyler Layne <skylerclayne@gmail.com>
2016-04-18 11:25:16 -0400

    docs

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,22 @@
+'use strict';
+
 const Writer = require('./Writer.js');
 const exec = require("child_process").exec;
+const Promise = require('promise');
 
 const dir = require('path').resolve(__dirname);
 const templates = `${dir}/templates`;
 
 const pkg =  process.argv[2];
 const writer = new Writer(templates, pkg);
-writer.parse();
+const promise = new Promise((resolve, reject) => {
+  resolve(writer.parse());
+});
 
-exec(`cd ${pkg}; npm install --save-dev`);
+promise.done(()=> {
+  console.log('Installing dependancies...');
+  exec(`cd ${pkg}; npm install`);
+  console.log('Creating executable scripts...');
+  exec(`chmod u+x ${pkg}/script/run`);
+  exec(`chmod u+x ${pkg}/script/test`);
+});
